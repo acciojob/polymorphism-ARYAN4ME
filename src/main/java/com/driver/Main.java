fa15dd85971739cb31f6d8a175195145720917ca
Mohit Prajapati <skparjapati9045@gmail.com>
2023-02-12 01:38:08 +0530

    my second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,28 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
     public static class Product{
         public int product(int x, int y) {
-            return x;
+            return x+y;
         }
         public int product(int x, int y, int z) {
-            return x;
+            return x+y+z;
         }
         public double product(double x, double y) {
-            return x;
+            return x+y;
         }
     }
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        double d = sc.nextDouble();
+        double e = sc.nextDouble();
         Product p = new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(1.0,2.0);
+        p.product(a,b);
+        p.product(a,b,c);
+        p.product(d,e);
     }
 
 }
